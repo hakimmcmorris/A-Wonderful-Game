52971d306d0494b4c45a464ae7fc00ff48ba8c51
Hakim McMorris <hakimmcmorris22@berkeley.edu>
2020-12-24 22:21:36 -0800

    Apple and Fruit Collected Script Finished
    Apple and FruitCollected.cs finished. Should make the apple a prefab and I need to finish the rest of the fruit. Will not be difficult. Just have to put the animations together.

---
+++ A Wonderful World/Assets/Scripts/FruitCollected.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitCollected : MonoBehaviour
+{
+    [SerializeField] Animator fruitAnimator;
+
+    bool collected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collected = false;
+        fruitAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        fruitAnimator.SetBool("Collected", collected);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !collected)
+        {
+            transform.localScale -= new Vector3(2, 2, 0);
+            collected = true;
+            Destroy(gameObject, fruitAnimator.GetCurrentAnimatorClipInfo(0).Length);
+        }
+    }
+}
