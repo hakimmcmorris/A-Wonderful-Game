21de5bc6f330d1e876a7b3392cc7cc6bc5da3223
Hakim McMorris <hakimmcmorris22@berkeley.edu>
2021-01-16 01:29:12 -0800

    Tutorial Level Progress
    Almost done finishing the level. It's coming along very well. It kinda looks professional not going to lie. I can't wait to see what it looks like when I'm all done.

--- A Wonderful World/Assets/Scripts/CameraScript.cs
+++ A Wonderful World/Assets/Scripts/CameraScript.cs
@@ -5,10 +5,34 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] GameObject Player;
+    bool connected;
+
+    private void Start()
+    {
+        connected = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+        if (connected)
+        {
+            transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+        }
+    }
+
+    public void Unconnect()
+    {
+        connected = false;
+    }
+
+    public void Reconnect()
+    {
+        connected = true;
+    }
+
+    public bool IsConnected()
+    {
+        return connected;
     }
 }
